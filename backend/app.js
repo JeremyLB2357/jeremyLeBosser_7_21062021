9ef05e2200507a78e5abc81595d2e6636afcb1b6
Le Bosser <jeremy.lebosser@gmail.com>
2021-06-22 10:12:57 +0200

    build [backend]: connexion à la BDD via sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('groupomania', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 
 const app = express();
 
-app.use((req, res) => {
+const userRoutes = require('./routes/users');
+const feedRoutes = require('./routes/feed');
+const profileRoutes = require('./routes/profile');
+
+async function connectBDD() {
+    try {
+        await sequelize.authenticate();
+        console.log('connection is great');
+    } catch (error) {
+        console.error('unable to connect to the database', error);
+    }
+};
+
+connectBDD();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/auth', userRoutes);
+app.use('/feed', feedRoutes);
+app.use('/profile', profileRoutes);
+
+/*app.use((req, res) => {
     res.json({ message: 'votre requête a bien été reçue !' });
-});
+});*/
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,4 @@ app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
+console.log('le serveur écoute sur le port 3000');
