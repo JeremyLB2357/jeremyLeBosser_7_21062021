74a8776ecaebac47642d9b24e1859db849ebb0c8
Le Bosser <jeremy.lebosser@gmail.com>
2021-07-06 10:02:54 +0200

    build: installation axios

--- frontend/social-front-app/src/components/Profile.vue
+++ frontend/social-front-app/src/components/Profile.vue
@@ -4,13 +4,26 @@
             <li>Nom</li>
             <li>Prémon</li>
             <li>Mail</li>
+            <li>Info: {{ info }}</li>
         </ul>
     </div>
 </template>
 
 <script>
+const axios = require('axios').default;
+
 export default {
     name: 'Profile',
+    data() {
+        return {
+            info: null
+        }
+    },
+    mounted() {
+        axios
+            .get('http://localhost:3000/api/profile/me?id=67bb971a-673b-4240-9b1d-67080d4dc915')
+            .then(response => (this.info = response))
+    }
 }
 </script>
 
